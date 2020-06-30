ffb187cc675067ee0313d96b67e7cd8f24593f18
tusar1986 <tusartkb@gmail.com>
2020-06-30 21:48:56 +0530

    Update TestRunner.java
    space removed from TestRunner class

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -5,7 +5,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
